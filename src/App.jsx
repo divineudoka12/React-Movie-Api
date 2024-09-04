14f7fd8b02112c01c5083a6e6c0f511c137d7621
mr.dee <132688882+divineudoka12@users.noreply.github.com>
2024-09-03 17:10:07 -0700

    second commir

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React,{useEffect, useState} from 'react'
-import "./App.css"
+import React, { useEffect, useState } from 'react'
 import Moviebox from './components/moviebox'
 
 function App() {
-  const[movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
@@ -11,78 +10,75 @@ function App() {
   const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=da112065e487c52791dd7a8e19982957";
   const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=da112065e487c52791dd7a8e19982957&query";
 
-  
 
-  useEffect(()=> {
+
+  useEffect(() => {
     setIsLoading(true)
-    fetch(API_URL).then((res)=>res.json())
-    .then(data=>{
-      setMovies(data.results)
-      setIsLoading(false)
-    })
+    fetch(API_URL).then((res) => res.json())
+      .then(data => {
+        setMovies(data.results)
+        setIsLoading(false)
+      })
   }, [])
 
   console.log(movies)
 
-  const searchMovie = async(e)=>{
+  const searchMovie = async (e) => {
     e.preventDefault();
-    try{
-      const url =`https://api.themoviedb.org/3/search/movie?api_key=da112065e487c52791dd7a8e19982957&query=${query}`;
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=da112065e487c52791dd7a8e19982957&query=${query}`;
       const res = await fetch(url);
       const data = await res.json();
       setMovies(data.results);
     }
-    catch(e){
+    catch (e) {
       console.log(e);
     }
   }
 
-  const changeHandler=(e)=>{
+  const changeHandler = (e) => {
     setQuery(e.target.value);
   }
 
 
   let content = null
 
-  if (isLoading === false && movies.length > 0){
-    content = 
-    <div className="container">
-      <div className="grid">
-        {movies.map((movieReq)=>
-        <Moviebox key={movieReq.id} {...movieReq}/>)}
+  if (isLoading === false && movies.length > 0) {
+    content =
+      <div className="w-full bg-black lg:px-16 px-6 py-4">
+        <div className=" m-auto grid lg:grid-cols-4 grid-cols-1 gap-4">
+          {movies.map((movieReq) =>
+            <Moviebox key={movieReq.id} {...movieReq} />)}
+        </div>
       </div>
-    </div>
   }
-  else if (isLoading === true && movies.length === 0){
+  else if (isLoading === true && movies.length === 0) {
     content = <p>Loading...</p>
   }
-  
-  else{
+
+  else {
     return
   }
 
   return (
-     <>
-    <header>
-    <div className="contianer">
+    <>
+          <nav className='w-full flex lg:flex-row flex-col bg-black justify-between items-center gap-6 lg:px-16 px-6 py-4 sticky top-0 z-50'>
+            <div>
+              <h1 className="text-white text-3xl font-semibold">Movies Info</h1>
+            </div>
 
-    <nav>
-        <div>
-        <h1 className="brand">Movies Info</h1>
-        </div>
+            <div className="flex justify-between gap-3">
+              <form onSubmit={searchMovie}>
+                <input className='border-none px-3 py-1 rounded-lg' type="text" placeholder="Enter a movie name" value={query} onChange={changeHandler} />
+              </form>
 
-       <form onSubmit={searchMovie} id="margintop">
-         <input type="text" placeholder="Search" value={query} onChange={changeHandler} />
-       <button className="btn-nav">Search</button>
-       </form>
+              <button className="bg-red-600 text-white font-semibold rounded-lg px-3 py-1">Search</button>
+            </div>
 
-     </nav>
-
-     </div>
-    </header>
+          </nav>
       {content}
-     </>
-   )
+    </>
+  )
 }
 
 export default App
--- src/components/moviebox.jsx
+++ src/components/moviebox.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 const API_IMG="http://image.tmdb.org/t/p/w500/"
-import "./moviebox.css"
 
 const moviebox= ({original_title, poster_path, vote_average, release_date, overview})=> {
     
@@ -10,24 +9,26 @@ const moviebox= ({original_title, poster_path, vote_average, release_date, overv
     const viewClose = () => setView(false);
 
     return (
-        <div className="card container">
-           <div className="card-body">
-               <img src={API_IMG+poster_path} className="card-img" />
-                <div className="card-btn">
-                   <div className="btn0"><button className="btn-open"  onClick={viewOpen}>View More</button></div>
-                {view && <div className="overlay">
-                     <div className="open" onSubmit={view} onClick={viewClose}>
-                        <img src={API_IMG+poster_path} className="card-inner" />
-                        <div className="card-text">
-                        <h3>{original_title}</h3>
-                        <h4>ImDb: {vote_average}</h4>
-                        <h5>Release Date: {release_date}</h5>
-                        <br /><br />
-                        <h6>Overview</h6>
-                        <p className="overview">{overview}</p>
-                        <footer>
-                        <button onClick={viewClose} className="btn-close">Close</button>
-                        </footer>
+        <div>
+           <div>
+               <div className='flex flex-col justify-between gap-4'>
+                <img src={API_IMG+poster_path} />
+                <button className="bg-red-600 text-white font-semibold rounded-lg px-3 py-1"  onClick={viewOpen}>View More</button>
+               </div>
+                <div className="">
+                {view && <div id="movie-modal" className="fixed top-6 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center ">
+                     <div className="bg-white rounded-lg p-6 max-w-md flex flex-col" onSubmit={view} onClick={viewClose}>
+                        <div className="text-black text-center">
+                            <div>
+                                <h3 className='text-2xl font-bold mb-4'>{original_title}</h3>
+                                <h4>ImDb: {vote_average}</h4>
+                                <h5>Release Date: {release_date}</h5>
+                                <h6 className='text-xl font-bold mb-4'>Overview</h6>
+                                <p className="text-[16px]">{overview}</p>
+                            </div>
+                            <div className='pt-3'>
+                                <button onClick={viewClose} className="bg-red-600 text-white font-semibold rounded-lg px-3 py-1">Close</button>
+                            </div>
                         </div>
                     </div>
                     </div>}
